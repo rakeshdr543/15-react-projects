cc6fec8735642b9831adeb4da52b25539761ce99
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-11 06:27:36 +0530

    Lorem paragraph

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,38 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+
+  const [count,setCount] = useState(0)
+  const [text,setText] = useState([])
   
-  
+  const handleSubmit =e=>{
+    e.preventDefault()
+
+    let paraCount = parseInt(count)
+    if(paraCount < 1){
+      paraCount = 1
+    }
+    if(paraCount > 8){
+      paraCount = 8
+    }
+    setText(data.slice(0,paraCount))
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className='section-center'>
+    <h3>tired of boring lorem ipsum?</h3>
+    <form className='lorem-form' onSubmit={handleSubmit} >
+    <label htmlFor='paragraph' >paragraphs:</label>
+    <input type='number' name='paragraph' id='paragraph' value={count} onChange={(e)=>setCount(e.target.value)} />
+    <button className='btn'>generate</button>
+    </form>
+    <article className='lorem-text'>
+      {text && text.map((item,index)=>{
+        return <p key={index}>{item}</p>
+      })}
+    </article>
+
+  </section>
     )
 }
 
