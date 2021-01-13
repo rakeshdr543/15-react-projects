74c9a6df8c523837031fe8b576993a8ddc91c423
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-13 07:15:29 +0530

    Added global context sidebar

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,35 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, createContext } from 'react'
+
+const AppContext = createContext()
+
+
+const AppProvider = ({children})=>{
+    
+const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+const [isModalOpen, setIsModalOpen] = useState(false);
+
+const openSidebar = () =>{
+    setIsSidebarOpen(true)
+}
+const closeSidebar = () =>{
+    setIsSidebarOpen(false)
+}
+
+const openModal = () =>{
+    setIsModalOpen(true)
+}
+const closeModal = () =>{
+    setIsModalOpen(false)
+}
+    return (
+        <AppContext.Provider value ={{isSidebarOpen,openSidebar,closeSidebar,setIsModalOpen,openModal,closeModal}}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+const useGlobalContext = ()=>{
+   return useContext(AppContext)
+}
+
+export {AppContext,AppProvider,useGlobalContext}
